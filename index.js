73f21d763400b5cdd7e3710af0f4cb71a706974f
Errieu <bartemplus@gmail.com>
2024-04-15 10:39:42 +0200

    Drivers/registration

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import client from './db.js';
 import registerRouter from './endpoints/user_management/register.js';
 import loginRouter from "./endpoints/user_management/login.js";
 import deleteRouter from "./endpoints/user_management/delete_account.js";
@@ -17,6 +16,7 @@ const PORT = 8000;
 const app = express();
 
 app.use(express.json());
+
 app.use('/register', registerRouter)
 app.use('/login', loginRouter)
 app.use('/users', deleteRouter);
@@ -29,16 +29,5 @@ app.use('/online', availableOrdersRouter);
 app.use('/online', acceptOrderRouter);
 app.use('/online', startTripRouter);
 app.use('/online', endTripRouter);
-
-app.get('/users', async (req, res) => {
-    try {
-        const result = await client.query('SELECT * FROM users');
-        const rows = result.rows;
-        res.status(200).json(rows);
-    } catch (error) {
-        console.error('Error executing query', error);
-        res.status(500).json('Internal Server Error');
-    }
-});
 
 app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
